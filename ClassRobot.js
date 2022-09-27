ef3cf363899bf76c7c9fe59ed99e6c6890d54a6c
SirUlric <114310315+SirUlric@users.noreply.github.com>
2022-09-27 12:55:54 +0200

    Add files via upload

---
+++ ClassRobot.js
@@ -0,0 +1,81 @@
+import { Dinosaurio } from "./ClassDinosaurios.js";
+
+/* Robot contra dinosaurios.
+El un tablero de 16x16 el sistema podrá generar n cantidad de dinosaurios.
+los dinosaurios se pueden mover en la cuadricula o atacar de manera aleatoria,
+ pero solo se podrán mover a espacios no ocupados por otros elementos.
+ El jugador podrá crear un robot y elegir en que parte de la cuadricula va nacer,
+  podrá atacar con 2 ataques y moverse pero solo tendrá 2 movimientos por turno.
+Ejemplo el usuario puede moverse 2 casilleros pero ya no podrá atacar,
+ pero si podía moverse 1 casilla y luego atacar o primero atacar luego moverse.
+Tampoco puede moverse a ningún casillero que esté ocupado.
+Elemento tiene su propio turno para hacer cosas.
+La representación de todo esto tendrá que ser por consola.
+Datos los dinosaurios de mueren con 2 golpes el robot se muere a los 5 golpes.
+Extra te animas a representar en la consola toda la cuadricula por cada turno? */
+export class Robot {
+    constructor(posicionX, posicionY) {
+        this.movimientosR = 0; //Movimientos Realizados
+        this.golpesRec = 0; //Golpes Recibidos
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+    }
+
+    get getMovimientosR() {
+        return this.movimientosR;
+    }
+    get getGolpesRec() {
+        return this.golpesRec;
+    }
+    get getPosicionX() {
+        return this.posicionX;
+    }
+    get getPosicionY() {
+        return this.posicionY;
+    }
+    get getIdentificador() {
+        return this.identificador;
+    }
+
+    set setPosicionX(posicion) {
+        this.posicionX = posicion;
+    }
+    set setPosicionY(posicion) {
+        this.posicionY = posicion;
+    }
+    set setMovimientosR(movimientos) {
+        this.movimientosR = movimientos;
+    }
+    set setIdentificador(numero) {
+        this.identificador = numero;
+    }
+
+    moverPX(cantidadCasilleros = 0, TABLERO) {
+        if(isNaN(cantidadCasilleros)) return console.log(`Se debe ingresar un valor numerico`);
+        if(cantidadCasilleros < -2 || cantidadCasilleros > 2) cantidadCasilleros = 2;
+        let pXV = this.getPosicionX;
+        let pX = this.getPosicionX + cantidadCasilleros;
+        let pY = this.getPosicionY;
+        if(this.getMovimientosR >= 2) return console.log(`Se supera la cantidad de movimientos por turno`);
+        if(TABLERO[pX][pY] instanceof Dinosaurio) return console.log(`Lugar ocupado por un Dinosaurio. [X: ${pX}][Y: ${pY}]`);
+        if(this.movimientosR + Math.abs(cantidadCasilleros) > 2) return console.log(`Solo se permiten 2 acciones por turno`);
+        this.setMovimientosR = this.movimientosR + Math.abs(cantidadCasilleros);
+        TABLERO[pX][pY] = this;
+        TABLERO[pX][pY].setPosicionX = pX;
+        TABLERO[pXV][pY] = null;
+    }
+    moverPY(cantidadCasilleros = 0, TABLERO) {
+        if(isNaN(cantidadCasilleros)) return console.log(`Se debe ingresar un valor numerico`);
+        if(cantidadCasilleros < -2 || cantidadCasilleros > 2) cantidadCasilleros = 2;
+        let pYV = this.getPosicionY;
+        let pY = this.getPosicionY + cantidadCasilleros;
+        let pX = this.getPosicionX;
+        if(this.movimientosR >= 2) return console.log(`Se supera la cantidad de movimientos por turno`);
+        if(TABLERO[pX][pY] instanceof Dinosaurio) return console.log(`Lugar ocupado por un Dinosaurio. [X: ${pX}][Y: ${pY}]`);
+        if(this.movimientosR + Math.abs(cantidadCasilleros) > 2) return console.log(`Solo se permiten 2 acciones por turno`);
+        this.setMovimientosR = this.movimientosR + Math.abs(cantidadCasilleros);
+        TABLERO[pX][pY] = this;
+        TABLERO[pX][pY].setPosicionY = pY;
+        if(TABLERO[pX][pYV] instanceof Robot) TABLERO[pX][pYV] = null;
+    }
+}
---
+++ DinosauriosVSRobots.js
@@ -0,0 +1,115 @@
+import { Robot } from './ClassRobot.js';
+import { Dinosaurio } from './ClassDinosaurios.js';
+
+const MAX_LUGARES = 64;
+const TABLERO = new Array(8);
+const recorrerTablero = (tablero, valor) => {
+    for(let x = 0; x < tablero.length; x++) {
+        for(let y = 0; y < tablero[x].length; y++) {
+            tablero[x][y] = valor;
+        }
+    }
+}
+
+//Creando tablero 8x8
+for(let i = 0; i < 8; i++) {
+    TABLERO[i] = new Array(8);
+};
+
+recorrerTablero(TABLERO, null);
+
+const asignarPosicionNueva = tablero => {
+    let pX = 0;
+    let pY = tablero[pX].findIndex(y => y === null);
+    while(pY === -1 && pX < 8) {
+        ++pX;
+        pY = tablero[pX].findIndex(y => y === null);
+    }
+    return [pX, pY];
+};
+
+const dinosauriosCreados = tablero => {
+    let dinosaurios = 0;
+    tablero.forEach((v, pX) => {
+        tablero[pX].forEach(pY => {
+            if(pY instanceof Dinosaurio) dinosaurios++;
+        })
+    });
+    console.log(`Se crearon ${dinosaurios} dinosaurios`);
+    return dinosaurios;
+}
+const crearRobot = (pX, pY) => {
+    //Creando Robot en el tablero
+    //El usuario elige donde crear el robot en el tablero
+    pX = parseInt(pX);
+    //Si los valores ingresados estan fuera de rango, se asignara una posicion por defecto
+    if(pX > 7 || pX < 0) { 
+        pX = 0;
+    }
+    pY = parseInt(pY);
+    if(pY > 7 || pY < 0) {
+        pY = 0;
+    }
+    //Si el lugar esta ocupado se asignara la primer posicion vacia
+    if(TABLERO[pX][pY] !== null) {
+        let posicionesNuevas = asignarPosicionNueva(TABLERO);
+        let pNX = posicionesNuevas[0]; //Posicion nueva X
+        let pNY = posicionesNuevas[1]; //Posicion nueva Y
+        return TABLERO[pNX][pNY] = new Robot(pNX, pNY);
+    }
+    TABLERO[pX][pY] = new Robot(pX, pY);
+    
+};
+
+const seleccionarUnidad = tablero => {
+    let r;
+    tablero.forEach((v, x) => {
+        tablero[x].forEach((v, y) => {
+            if(tablero[x][y] instanceof Robot) return r = tablero[x][y];
+        })
+    });
+    return r;
+}
+
+const moverRobot = (pX, pY) => {
+    let r = seleccionarUnidad(TABLERO);
+    if(r.getPosicionX + pX < 0 || r.getPosicionX + pX > 7) return console.log(`Lugar fuera de rango`);
+    if(r.getPosicionY + pY < 0 || r.getPosicionY + pY > 7) return console.log(`Lugar fuera de rango`);
+    if(r.movimientosR >= 2) return console.log(`No puedes realizar mas movimientos. Espera tu turno`);
+    r.moverPX(pX, TABLERO);
+    r.moverPY(pY, TABLERO);
+};
+
+
+
+//Creando Dinosaurios en posiciones aleatorias sobre el tablero
+(function() {
+        let cantidadDinosaurios = Math.round(Math.random() * (60 - 1) + 1);
+        if(cantidadDinosaurios >= MAX_LUGARES) cantidadDinosaurios--;
+        for(let i = 1; i <= cantidadDinosaurios; i++) {
+            let nuevoDinosaurio = new Dinosaurio();
+            //Obteniendo posiciones para asignar en el tablero
+            let pX = nuevoDinosaurio.getPosicionX; //Guardando posicion en variable
+            let pY = nuevoDinosaurio.getPosicionY; //Guardando posicion en variable
+            //Si el lugar esta ocupado, se asigna la primera posicion vacia
+            if(TABLERO[pX][pY] !== null) {
+                let posicionesNuevas = asignarPosicionNueva(TABLERO);
+                let pNX = posicionesNuevas[0]; //Posicion nueva X
+                let pNY = posicionesNuevas[1]; //Posicion nueva Y
+                nuevoDinosaurio.setPosicionX = pNX; //Se asigna posicion nueva 
+                nuevoDinosaurio.setPosicionY = pNY; //Se asigna posicion nueva
+                TABLERO[pNX][pNY] = nuevoDinosaurio;
+            }
+            TABLERO[pX][pY] = nuevoDinosaurio;
+        }
+    }
+)();
+
+const verTablero = () => console.table(TABLERO);
+
+
+crearRobot(7, 2);
+verTablero();
+moverRobot(-1, 1);
+verTablero();
+console.log(seleccionarUnidad(TABLERO));
